5138b2e3f53d5549b08dfe69f6f1c158c1690919
tanveer128423 <khalidtanveer653@gmail.com>
2026-01-12 20:35:44 +0530

    Final changes done and improved UI

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 // Routes
@@ -24,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("Expense Tracker Backend Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
